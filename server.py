90fc1aa0d278c8e3ff466ddbe59a3c5a780ac227
Bart Jeukendrup <bart@jeukendrup.nl>
2022-07-04 16:45:39 +0200

    Add openbareRuimteNaam debug statement

--- server.py
+++ server.py
@@ -59,6 +59,7 @@ def index():
 
     adres = None
     if betreftAdres and gerelateerde and adresAanduidingGrp and adresAanduidingGrp['http://www.egem.nl/StUF/sector/bg/0310:gor.openbareRuimteNaam'] and adresAanduidingGrp['http://www.egem.nl/StUF/sector/bg/0310:wpl.woonplaatsNaam']:
+        print(adresAanduidingGrp['http://www.egem.nl/StUF/sector/bg/0310:gor.openbareRuimteNaam'])
         adres = {
             'openbare_ruimte': adresAanduidingGrp['http://www.egem.nl/StUF/sector/bg/0310:gor.openbareRuimteNaam'],
             'huisnummer': adresAanduidingGrp.get('http://www.egem.nl/StUF/sector/bg/0310:aoa.huisnummer', ''),
